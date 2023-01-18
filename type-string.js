237c582467fc61a7e6f3b931a60d2c3288d3a130
Bruna Machado <fmachadobruna@gmail.com>
2023-01-18 20:04:00 -0300

    Initial commit

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNúmeros = "3456";
+
+const citacao = "Meu nome é ";
+const meuNome = "Bruna";
+
+// concatenacao (+)
+
+console.log(citacao + meuNome)
+
+// template string ou template literal
